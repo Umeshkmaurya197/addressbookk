445ef10addf4c6eb5fb5417890b142e6bbdb7dd9
Umeshkmaurya197 <umeshkumarmaurya.98@gmail.com>
2022-07-18 06:46:14 +0530

    Refactored : AddressBookMain.java new contact added

--- src/com/addressbook/AddressBookMain.java
+++ src/com/addressbook/AddressBookMain.java
@@ -1,10 +1,49 @@
 package com.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 
 	public static void main(String[] args) {
 		System.out.println("------- Welcome to Address Book Program -------- ");
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print(" Enter  first name : ");
+		String firstName = scanner.next();
+
+		System.out.print(" Enter last name : ");
+		String lastName = scanner.next();
+
+		System.out.print(" Enter address : ");
+		String address = scanner.next();
+
+		System.out.print(" Enter city : ");
+		String city = scanner.next();
+
+		System.out.print(" Enter state : ");
+		String state = scanner.next();
+
+		System.out.print(" Enter zip : ");
+		int zip = scanner.nextInt();
+
+		System.out.print(" Enter phone number : ");
+		long phoneNumber = scanner.nextLong();
+
+		System.out.print(" Enter email : ");
+		String email = scanner.next();
+
+		AddressBook addressBook = new AddressBook(firstName, lastName, address, city, state, zip, phoneNumber, email);
+	
+		System.out.println(" First Name : "+addressBook.getFirstName());
+		System.out.println(" Last Name  : "+addressBook.getLastName());
+		System.out.println(" Address    : "+addressBook.getAddress());
+		System.out.println(" City       : "+addressBook.getCity());
+		System.out.println(" State      : "+addressBook.getState());
+		System.out.println(" Zip        : "+addressBook.getZip());
+		System.out.println(" PhoneNumber: "+addressBook.getPhoneNumber());
+		System.out.println(" email      : "+addressBook.getEmail());
+	
 	}
-	
-	
+
 }
