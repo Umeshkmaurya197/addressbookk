c845596a7fba2f185640a1064e07fabac7e01fb3
Umeshkmaurya197 <umeshkumarmaurya.98@gmail.com>
2022-07-18 06:13:40 +0530

    Added : AddressBook.java for addressbook data file added

---
+++ src/com/addressbook/AddressBook.java
@@ -0,0 +1,86 @@
+package com.addressbook;
+
+public class AddressBook {
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String state;
+	private int zip;
+	private long phoneNumber;
+	private String email;
+	
+	public AddressBook(String firstName, String lastName, String address, String state, int zip, long phoneNumber,
+			String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "AddressBook [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", state="
+				+ state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email=" + email + "]";
+	}
+
+}
